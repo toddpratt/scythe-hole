4d9c620450ba51733f246d4ad4a625dab6e93141
Glitch (scythe-hole) <none>
2018-06-13 01:05:10 +0000

    🍩🌶 Updated with Glitch

--- public/client.js
+++ public/client.js
@@ -1,3 +1,47 @@
 
 $(function() {
+  let template = Handlebars.compile($("#messages-template").html());
+  
+  function fetchMessages() {
+    $.get('/messages/asdf', messages => {
+      var msgsDiv = $("#messages");
+      msgsDiv.empty().html(template({
+        messages: messages,
+        changeMe: 1
+      }));
+      msgsDiv.css("top", 440 - msgsDiv.height());
+    });
+    setTimeout(fetchMessages, 5000);
+  }
+  fetchMessages();
+
+  if(location.pathname == "/") {
+    $("#no-chan").show();
+  } else {
+    $("no-chan").hide();
+  }
+  
+  if(localStorage.nick) {
+    $("#username").val(localStorage.nick);
+  } else {
+    $(".message-container").hide();
+  }
+  
+  $("#username").change(d => {
+    localStorage.nick = $("#username").val();
+    if(localStorage.nick) {
+      $(".message-container").show();
+    }
+  });
+  
+  $("#message").keypress(ev => {
+    if(ev.which == 13) {
+      var msg = $("#message").val();
+      var nick = $("#username").val();
+
+      if(msg.length > 0) {
+        $.post('/message' + location.pathname, {nick: nick, message: msg});
+      }
+    }
+  });
 });
--- server.js
+++ server.js
@@ -1,17 +1,65 @@
 
 const express = require('express');
-const irc = require('irc');
+const bodyParser = require('body-parser');
+const request = require('request');
 
+
+class MessageList {
+  constructor() {
+    this.messages = [];
+  }
+  
+  add(nick, message) {
+    var timestamp = new Date();
+    this.messages.push({nick, message, timestamp});
+    if(this.messages.length > 10) {
+      this.messages = this.messages.slice(this.messages.length - 10);
+    }
+  }
+};
+
+var channels = {};
 const app = express();
 
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 app.use(express.static('public'));
 
-app.get("/", function (request, response) {
-  response.sendFile(__dirname + '/views/index.html');
+app.get("/:id", function (req, res) {
+  res.sendFile(__dirname + '/views/index.html');
 });
 
-app.get("/messages", (req, res, next) => {
-  res.json(messages);
+app.get("/", function (req, res) {
+  if(req.params.id) {
+    channels[req.params.id] = new MessageList();
+  }
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+app.get("/messages/:id", (req, res, next) => {
+  var id = "abc";
+  if(!id || !channels[id]) {
+    res.status(500).send("channel not found");
+    return;
+  }
+  res.send(channels[id].messages);
+});
+
+app.post("/message/:id", (req, res, next) => {
+  var id = res.params.id;
+  if(!req.body.nick || req.body.nick.length < 1) {
+    res.status(500).send("no nickname specified");
+  } else if(!req.body.message || req.body.message.length < 1) {
+    res.status(500).send("no message specified");
+  } else if(!channels[id]) {
+    res.status(500).send("no message specified");
+  } else {
+    if(!id) {
+      channels[id] = new MessageList();
+    }
+    channels[id].add(req.body.nick, req.body.message);
+    res.status(200).send("message received");
+  }
 });
 
 app.listen(3000);
